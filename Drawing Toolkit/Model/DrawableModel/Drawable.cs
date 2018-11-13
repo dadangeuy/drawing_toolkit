93fbb2cc9ee9fe325840f43bfa2782ced4e642ad
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-11-13 17:50:01 +0700

    Refactor

--- Drawing Toolkit/Model/DrawableModel/Drawable.cs
+++ Drawing Toolkit/Model/DrawableModel/Drawable.cs
@@ -1,34 +1,18 @@
 ﻿using System.Drawing;
 using Drawing_Toolkit.Common;
+using Drawing_Toolkit.Model.DrawableModel.Event;
 using Drawing_Toolkit.Model.DrawableModel.State;
 
 namespace Drawing_Toolkit.Model.DrawableModel {
-    abstract class Drawable : StateContext<DrawingState> {
+    abstract partial class Drawable : StateContext<DrawingState> {
+        public static readonly Drawable EMPTY = new EmptyDrawingContext();
+
         public Drawable() : base(EditState.INSTANCE) { }
+
         public abstract bool Intersect(Point location);
         public abstract bool Intersect(Rectangle area);
         public abstract void Move(Point offset);
         public abstract void Resize(Point from, Point to);
         public abstract void Render(Graphics graphics);
-
-        public static readonly Drawable EMPTY = new EmptyDrawingContext();
-        private class EmptyDrawingContext : Drawable {
-            public override bool Intersect(Point location) {
-                return false;
-            }
-
-            public override bool Intersect(Rectangle area) {
-                return false;
-            }
-
-            public override void Move(Point offset) {
-            }
-
-            public override void Render(Graphics graphics) {
-            }
-
-            public override void Resize(Point from, Point to) {
-            }
-        }
     }
 }
---
+++ Drawing Toolkit/Model/DrawableModel/EmptyDrawingContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace Drawing_Toolkit.Model.DrawableModel {
+    class EmptyDrawingContext : Drawable {
+        public override bool Intersect(Point location) {
+            return false;
+        }
+
+        public override bool Intersect(Rectangle area) {
+            return false;
+        }
+
+        public override void Move(Point offset) {
+        }
+
+        public override void Render(Graphics graphics) {
+        }
+
+        public override void Resize(Point from, Point to) {
+        }
+    }
+}
